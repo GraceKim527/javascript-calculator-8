3725d2bc60a1f49354671738e428846bfc3f7c10
gracekim527 <k07173027@gmail.com>
2025-10-16 20:14:36 +0900

    :sparkles: Feat(parser): 쉼표 또는 클론으로 구분된 숫자의 합 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  checkEmptyString(input) {
+    if (input === "") {
+      throw new Error("[ERROR] 빈 문자열은 입력할 수 없습니다.");
+    }
+  }
+
+  add(input) {
+    this.checkEmptyString(input);
+    if (input.length === 1) {
+      return Number(input);
+    }
+    const tokens = input.split(/,|:/);
+    return tokens.reduce((acc, curr) => acc + Number(curr), 0);
+  }
+
+  async run() {
+    const input = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해주세요. \n"
+    );
+    try {
+      this.checkEmptyString(input);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+      return;
+    }
+    Console.print(`결과 : ${this.add(input)}`);
+  }
 }
 
 export default App;
